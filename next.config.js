5c1dbcaea82025388b0511f02e065f96070dd3f3
jastiling <jastiling@gmail.com>
2022-09-11 12:16:00 -0700

    switch over to typescript and start changing readme

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  typescript: {
+    tsconfigPath: "./tsconfig.json",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
